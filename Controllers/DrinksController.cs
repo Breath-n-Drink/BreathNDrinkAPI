b0296358c9b80fd44137ba3210f3fb0748d14eb5
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-11-22 13:19:48 +0100

    Retriever

--- Controllers/DrinksController.cs
+++ Controllers/DrinksController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using BreathNDrinkAPI.Managers;
 using BreathNDrinkClassLibrary;
---
+++ Retrievers/DrinksRetriever.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BreathNDrinkAPI.Retrievers
+{
+    public class DrinksRetriever
+    {
+        private static HttpClient client = new HttpClient();
+        private static string path = "http://www.thecocktaildb.com/api/json/v1/1/search.php";
+
+        public static async Task<string> GetDrinkByNameAsync(string name)
+        {
+            HttpResponseMessage response = await client.GetAsync(path + $"?s={name}");
+            string jsonDrink = "";
+            if (response.IsSuccessStatusCode)
+            {
+                jsonDrink = await response.Content.ReadAsStringAsync();
+            }
+            return jsonDrink;
+        }
+
+        public static async Task<string> GetDrinkByIdAsync(string id)
+        {
+            HttpResponseMessage response = await client.GetAsync(path + $"i={id}");
+            string jsonDrink = "";
+            if (response.IsSuccessStatusCode)
+            {
+                jsonDrink = await response.Content.ReadAsStringAsync();
+            }
+            return jsonDrink;
+        }
+    }
+}
