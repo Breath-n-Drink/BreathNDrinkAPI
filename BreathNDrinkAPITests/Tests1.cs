672caa2d79a765fe0f51f334af97cc05fb14bfcb
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-11-22 10:08:50 +0100

    Add project files.

---
+++ BreathNDrinkAPITests/Tests1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BreathNDrinkAPITests
+{
+    [TestClass]
+    public class Tests1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
