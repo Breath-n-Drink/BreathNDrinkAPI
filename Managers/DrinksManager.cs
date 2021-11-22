4b7873787ede30756c6749309e4d8d972eed2262
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-11-22 10:58:18 +0100

    Manager

---
+++ Managers/DrinksManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BreathNDrinkClassLibrary;
+
+namespace BreathNDrinkAPI.Managers
+{
+    public static class DrinksManager
+    {
+        private static List<Drink> _drinksList = new();
+
+        public static void AddDrink(Drink drink)
+        {
+            _drinksList.Add(drink);
+        }
+
+        public static void RemoveDrink(string drinkId)
+        {
+            _drinksList.Remove(_drinksList.Find(d => d.DrinkId.Equals(drinkId)));
+        }
+    }
+}
