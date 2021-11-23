cac0264087d2eb3362f0af215c597bc92b62dbb4
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2021-11-23 09:29:54 +0100

    Simplified if statements in Retriever

--- Retrievers/DrinksRetriever.cs
+++ Retrievers/DrinksRetriever.cs
@@ -51,79 +51,36 @@ namespace BreathNDrinkAPI.Retrievers
             drink.ImgThumbUrl = tempDrink.StrDrinkThumb;
             drink.Instructions = tempDrink.StrInstructions;
 
-            if (tempDrink.StrIngredient1 != null)
-            {
-                drink.AddIngredientAndMeasurement(tempDrink.StrIngredient1, tempDrink.StrMeasure1);
-                if (tempDrink.StrIngredient2 != null)
-                {
-                    drink.AddIngredientAndMeasurement(tempDrink.StrIngredient2, tempDrink.StrMeasure2);
-                    if (tempDrink.StrIngredient3 != null)
-                    {
-                        drink.AddIngredientAndMeasurement(tempDrink.StrIngredient3, tempDrink.StrMeasure3);
-                        if (tempDrink.StrIngredient4 != null)
-                        {
-                            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient4, tempDrink.StrMeasure4);
-                            if (tempDrink.StrIngredient5 != null)
-                            {
-                                drink.AddIngredientAndMeasurement(tempDrink.StrIngredient5, tempDrink.StrMeasure5);
-                                if (tempDrink.StrIngredient6 != null)
-                                {
-                                    drink.AddIngredientAndMeasurement(tempDrink.StrIngredient6, tempDrink.StrMeasure6);
-                                    if (tempDrink.StrIngredient7 != null)
-                                    {
-                                        drink.AddIngredientAndMeasurement(tempDrink.StrIngredient7,
-                                            tempDrink.StrMeasure7);
-                                        if (tempDrink.StrIngredient8 != null)
-                                        {
-                                            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient8,
-                                                tempDrink.StrMeasure8);
-                                            if (tempDrink.StrIngredient9 != null)
-                                            {
-                                                drink.AddIngredientAndMeasurement(tempDrink.StrIngredient9,
-                                                    tempDrink.StrMeasure9);
-                                                if (tempDrink.StrIngredient10 != null)
-                                                {
-                                                    drink.AddIngredientAndMeasurement(tempDrink.StrIngredient10,
-                                                        tempDrink.StrMeasure10);
-                                                    if (tempDrink.StrIngredient11 != null)
-                                                    {
-                                                        drink.AddIngredientAndMeasurement(tempDrink.StrIngredient11,
-                                                            tempDrink.StrMeasure11);
-                                                        if (tempDrink.StrIngredient12 != null)
-                                                        {
-                                                            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient12,
-                                                                tempDrink.StrMeasure12);
-                                                            if (tempDrink.StrIngredient13 != null)
-                                                            {
-                                                                drink.AddIngredientAndMeasurement(
-                                                                    tempDrink.StrIngredient13,
-                                                                    tempDrink.StrMeasure13);
-                                                                if (tempDrink.StrIngredient14 != null)
-                                                                {
-                                                                    drink.AddIngredientAndMeasurement(
-                                                                        tempDrink.StrIngredient14,
-                                                                        tempDrink.StrMeasure14);
-                                                                    if (tempDrink.StrIngredient15 != null)
-                                                                    {
-                                                                        drink.AddIngredientAndMeasurement(
-                                                                            tempDrink.StrIngredient15,
-                                                                            tempDrink.StrMeasure15);
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
+            if (tempDrink.StrIngredient1 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient1, tempDrink.StrMeasure1);
+            if (tempDrink.StrIngredient2 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient2, tempDrink.StrMeasure2);
+            if (tempDrink.StrIngredient3 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient3, tempDrink.StrMeasure3);
+            if (tempDrink.StrIngredient4 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient4, tempDrink.StrMeasure4);
+            if (tempDrink.StrIngredient5 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient5, tempDrink.StrMeasure5);
+            if (tempDrink.StrIngredient6 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient6, tempDrink.StrMeasure6);
+            if (tempDrink.StrIngredient7 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient7, tempDrink.StrMeasure7);
+            if (tempDrink.StrIngredient8 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient8, tempDrink.StrMeasure8);
+            if (tempDrink.StrIngredient9 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient9, tempDrink.StrMeasure9);
+            if (tempDrink.StrIngredient10 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient10, tempDrink.StrMeasure10);
+            if (tempDrink.StrIngredient11 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient11, tempDrink.StrMeasure11);
+            if (tempDrink.StrIngredient12 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient12, tempDrink.StrMeasure12);
+            if (tempDrink.StrIngredient13 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient13, tempDrink.StrMeasure13);
+            if (tempDrink.StrIngredient14 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient14, tempDrink.StrMeasure14);
+            if (tempDrink.StrIngredient15 == null) return drink;
+            drink.AddIngredientAndMeasurement(tempDrink.StrIngredient15, tempDrink.StrMeasure15);
             return drink;
         }
     }
