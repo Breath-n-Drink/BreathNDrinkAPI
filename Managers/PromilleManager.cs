13b04eab8756ee40567b5cd372749b3995600dae
MadsBruhn <mads80i4@edu.easj.dk>
2021-11-23 13:36:06 +0100

    Lavede get promille i api'en, som får den sidste promille i databasen

---
+++ Managers/PromilleManager.cs
@@ -0,0 +1,16 @@
+﻿using BreathNDrinkAPI.Models;
+using System.Linq;
+
+namespace BreathNDrinkAPI.Managers
+{
+    public class PromilleManager
+    {
+        private BreathndrinkContext _dbContext = new BreathndrinkContext();
+
+        public double GetPromillle()
+        {
+            Promille result = _dbContext.Promille.ToList()[^1];
+            return result.Promille1;
+        }
+    }
+}
