c17b0aa955b29230462ed11bd519bffbbad58920
Sebastian Løgtholt Westergaard <seba255j@edu.easj.dk>
2021-12-07 12:34:30 +0100

    favoritefix

--- Managers/FavoriteDrinksManager.cs
+++ Managers/FavoriteDrinksManager.cs
@@ -12,11 +12,16 @@ namespace BreathNDrinkAPI.Managers
 
         public static void AddFavorite(int drinkId, int drinkerId)
         {
-            FavoriteDrinks newFavorite = new FavoriteDrinks();
-            newFavorite.DrinkId = drinkId;
-            newFavorite.DrinkerId = drinkerId;
-            _dbContext.Add(newFavorite);
-            _dbContext.SaveChanges();
+            if (_dbContext.FavoriteDrinks.Where(d => d.DrinkId == drinkId && d.DrinkerId == drinkerId).ToList().Count==0)
+            {
+                FavoriteDrinks newFavorite = new FavoriteDrinks();
+                newFavorite.DrinkId = drinkId;
+                newFavorite.DrinkerId = drinkerId;
+
+                _dbContext.Add(newFavorite);
+                _dbContext.SaveChanges();
+            }
+                
         }
 
         public static void DeleteFavorite(int drinkId, int drinkerId)
